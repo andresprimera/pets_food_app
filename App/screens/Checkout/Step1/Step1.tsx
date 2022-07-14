3c432e78c38b1c942cf9a9f049371c6c24672733
andresprimera <andresprimera@gmail.com>
2022-07-14 08:17:11 -0500

    Making the continue checkout button persistent

--- App/screens/Checkout/Step1/Step1.tsx
+++ App/screens/Checkout/Step1/Step1.tsx
@@ -8,26 +8,13 @@ import {
   TouchableWithoutFeedback,
 } from 'react-native';
 
-import {useNavigation} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
-import {CheckoutStackParamList} from '../CheckoutRouteTypes';
-
-import {Button} from '@app/components/atoms/Button';
 import {AddressSelection} from './AddressSelection';
 import {ShipmentInfo} from './ShipmentInfo';
 import {styles} from './styles';
 
 import {InputDatePicker} from '@app/components/atoms/InputDatePicker';
 
-type checkoutProps = StackNavigationProp<CheckoutStackParamList, 'Step2'>;
-
-function handleNavigate(navigation: checkoutProps) {
-  navigation.navigate('Step2');
-}
-
 export const Step1 = () => {
-  const navigation = useNavigation<checkoutProps>();
-
   return (
     <KeyboardAvoidingView
       keyboardVerticalOffset={250}
@@ -40,11 +27,6 @@ export const Step1 = () => {
           </View>
           <ShipmentInfo />
           <AddressSelection />
-          <Button
-            propStyle={styles.button}
-            text={'Siguiente'}
-            onPress={() => handleNavigate(navigation)}
-          />
         </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
--- App/screens/Checkout/Step3/Step3.tsx
+++ App/screens/Checkout/Step3/Step3.tsx
@@ -1,37 +1,19 @@
 import React from 'react';
 
 import {View} from 'react-native';
-
-import {useNavigation} from '@react-navigation/native';
-import {StackNavigationProp} from '@react-navigation/stack';
-import {CheckoutStackParamList} from '../CheckoutRouteTypes';
-
 import {ProductSummary} from './ProductSummary';
 import {styles} from './styles';
-import {Button} from '@app/components/atoms/Button';
 import {DeliveryPrice} from './DeliveryPrice';
 import {Divider} from '@app/components/atoms/Divider';
 import {FinalSection} from './FinalSection';
 
-type checkoutProps = StackNavigationProp<CheckoutStackParamList, 'Step3'>;
-
-function handleNavigate(navigation: checkoutProps) {
-  navigation.navigate('Step3');
-}
-
 export const Step3 = () => {
-  const navigation = useNavigation<checkoutProps>();
   return (
     <View style={styles.container}>
       <ProductSummary />
       <Divider />
       <DeliveryPrice />
       <FinalSection />
-      <Button
-        propStyle={styles.button}
-        text={'Finalizar compra'}
-        onPress={() => handleNavigate(navigation)}
-      />
     </View>
   );
 };
